f9e5f53b56f4cbc23b1b09a83d18ac0af37f2071
Robert <kaczmarekrob13@gmail.com>
2017-03-15 08:39:34 +0100

    pierwszy

---
+++ app/src/main/java/com/example/robertkaczmarek/wynagrodzeniacalculator/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.robertkaczmarek.wynagrodzeniacalculator;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        AdapterView.OnItemClickListener salaryListener = new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+              if (i == 0){
+                  Intent salaryIntent = new Intent(MainActivity.this, SalaryActivity.class);
+                  startActivity(salaryIntent);
+
+
+              }
+                else if (i ==1){
+                  Intent holidayIntent = new Intent(MainActivity.this, HolidayActivity.class);
+                  startActivity(holidayIntent);
+              }
+                else if (i==2){
+                  Uri pip = Uri.parse("http://www.pip.gov.pl");
+                  Intent sendpip = new Intent(Intent.ACTION_VIEW, pip);
+                  startActivity(sendpip);
+
+                  }
+                else{
+                    Uri zus = Uri.parse("http://www.zus.pl");
+                    Intent sendzus = new Intent(Intent.ACTION_VIEW, zus);
+                    startActivity(sendzus);
+                }
+
+              }
+
+
+        };
+
+        ListView list = (ListView)findViewById(R.id.list);
+        list.setOnItemClickListener(salaryListener);
+
+
+
+    }
+}
---
+++ app/src/test/java/com/example/robertkaczmarek/wynagrodzeniacalculator/List.java
@@ -0,0 +1,11 @@
+package com.example.robertkaczmarek.wynagrodzeniacalculator;
+
+import android.app.Activity;
+
+/**
+ * Created by robertkaczmarek on 05.03.2017.
+ */
+
+public class List extends Activity {
+
+}
